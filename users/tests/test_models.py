54983ae5cb22b6adb03ab705e9b0ec48b64939a4
Me <edu.kaelteritter@yandex.ru>
2025-04-23 12:06:43 +0400

    Тестирование User: продолжение
    1. Создан модульный тест для проверки строкового
    представления User
    2. Создан модульный тест для проверки наличия
    человекочитаемого имени User
    3. Корретировка User под тесты

--- users/tests/test_models.py
+++ users/tests/test_models.py
@@ -13,9 +13,23 @@ class UserModelTest(TestCase):
             password='!changeMe'
         )
 
-
     def test_user_can_be_created(self):
         '''
         Тест: Пользователь создается (кастомная версия)
         '''
         self.assertTrue(User.objects.filter(username='exampleuser').exists())
+
+    def test_string_representation(self):
+        '''
+        Тест: User имеет строковое представление
+        '''
+        self.assertTrue(hasattr(self.user, '__str__'))
+
+    def test_verbose_name(self):
+        '''
+        Тест: модель User имеет человекочитаемое имя
+        '''
+        self.assertTrue(hasattr(self.user._meta, 'verbose_name'))
+        self.assertTrue(hasattr(self.user._meta, 'verbose_name_plural'))
+        self.assertEqual(self.user._meta.verbose_name, 'Пользователь')
+        self.assertEqual(self.user._meta.verbose_name_plural, 'Пользователи')
