a7e5cc2ecaaaea0e80b9ac93b90f4f95dfc83666
Me <edu.kaelteritter@yandex.ru>
2025-04-23 12:36:45 +0400

    Поле фотографии для User
    1. User.photo: добавлено и протестировано
    на возможность загрузки фотографии
    2. Создана функция для определения пути
    хранения медиа-файлов от пользователей
    3. Указаны настройки для хранения медиа файлов

--- users/models.py
+++ users/models.py
@@ -1,11 +1,19 @@
-from django.contrib.auth.models import AbstractUser, User
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+def get_user_directory_path(instance, filename):
+    return f'users/{instance.id}/photos/{filename}'
+
+
 class User(AbstractUser):
-    # username = ...
-    # email = ...
-    # photo = ...
+    photo = models.ImageField(
+        verbose_name='Аватар',
+        upload_to=get_user_directory_path,
+        blank=True,
+        null=True
+    )
+
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
--- users/tests/test_models.py
+++ users/tests/test_models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import TestCase
 
 
@@ -33,3 +34,21 @@ class UserModelTest(TestCase):
         self.assertTrue(hasattr(self.user._meta, 'verbose_name_plural'))
         self.assertEqual(self.user._meta.verbose_name, 'Пользователь')
         self.assertEqual(self.user._meta.verbose_name_plural, 'Пользователи')
+
+    def test_photo_can_be_uploaded(self):
+        '''
+        Тест: Для модели пользователя загружается фотография
+        '''
+        pic = (
+        b'\x47\x49\x46\x38\x39\x61\x01\x00\x01\x00\x80\x00\x00\xff\xff\xff'
+        b'\xff\xff\xff\x21\xf9\x04\x01\x00\x00\x00\x00\x2c\x00\x00\x00\x00'
+        b'\x01\x00\x01\x00\x00\x02\x02\x44\x01\x00\x3b'
+        )
+        gif_file = SimpleUploadedFile(
+            'test_image.gif',
+            pic,
+            content_type='image/gif'
+        )
+        new_user = User.objects.create_user(username='testuser2', photo=gif_file)
+        with open(new_user.photo.path, 'rb') as f:
+            self.assertEqual(f.read(), pic)
