727265cdc3fe2e9d6a302c90f1e319182f5b540e
Me <edu.kaelteritter@yandex.ru>
2025-04-28 17:56:42 +0400

    Добавление кнопки регистрации и изменение лейблов
    1. Названия полей теперь на русском
    2. Добавлена надпись на кнопке

--- users/forms.py
+++ users/forms.py
@@ -5,8 +5,11 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class SignUpForm(forms.ModelForm):
-    password1 = forms.CharField(widget=forms.PasswordInput)
-    password2 = forms.CharField(widget=forms.PasswordInput)
+    password1 = forms.CharField(widget=forms.PasswordInput, label='Пароль')
+    password2 = forms.CharField(widget=forms.PasswordInput, label='Подтверждения пароля')
     class Meta:
         model = User
         fields = ['username', 'password1', 'password2']
+        labels = {
+            'username': 'Пользователь',
+        }
