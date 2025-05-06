fda1fd9471f8e4197a43bc1c2bf18424f8e16c01
Me <edu.kaelteritter@yandex.ru>
2025-05-06 10:57:46 +0400

    Выведены все посты на главную страницу
    1. Исправлен контроллер главной страницы:
    дан контекст из объектов поста

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
+from .models import Post
+
 
 def home(request):
-    return render(request, 'index.html')
+    posts = Post.objects.all()
+    context = {
+        'posts': posts
+    }
+    return render(request, 'index.html', context=context)
